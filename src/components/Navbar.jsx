8b034a34df9f09084e9a836067c49a47eb03b8e1
Alexis Winters <alexiscodes@yahoo.com>
2023-04-10 23:33:01 -0400

    created navbar, footer, and routes components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="p-5 pb-0 d-flex flex-wrap justify-content-sm-between justify-content-center align-items-center border-bottom">
+      <h1>Navbar</h1>
+    </div>
+  );
+};
