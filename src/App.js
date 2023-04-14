3d222fc4d94307edf087be9fc91dceed37523bad
Alexis Winters <alexiscodes@yahoo.com>
2023-04-14 13:55:57 -0400

    installed material UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div className="app">
       <h1>Google Clone</h1>
+
+      { /* Home (the one with the search on */}
+      <Home />
+
+      {/* SearchPage (the results page */}
+
     </div>
   );
 };
