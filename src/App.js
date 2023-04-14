1e45c2c5fbeacabebefe2958aa598a556a240ebb
Alexis Winters <alexiscodes@yahoo.com>
2023-04-14 12:23:53 -0400

    restart project without bootstrap, uninstalled bootstrap and reactstrap

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { useState } from "react";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
-import { Routes } from "./components/Routes";
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className={darkTheme ? "dark" : ""}>
-      <div className="bg-secondary bg-opacity-10 dark:bg-opacity-75 dark:text-white h-auto">
-        <Navbar />
-        <Routes />
-        <Footer />
-      </div>
+    <div className="app">
+      <h1>Google Clone</h1>
     </div>
   );
 };
