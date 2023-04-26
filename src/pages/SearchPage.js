4676704ed32a1476979e088a8197cba6f26ecf26
Alexis Winters <alexiscodes@yahoo.com>
2023-04-26 06:32:38 -0500

    hideButtons, response, api key, context key

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./SearchPage.css";
-import { useStateValue } from '../StateProvider';
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
+import Response from '../response';
 
 const SearchPage = () => {
-
   const [{ term }, dispatch] = useStateValue();
+
+  // LIVE API CALL
+  // const { data } = useGoogleSearch(term)
+
+  const data = Response;
+
+  // https://developers.google.com/custom-search/v1/using_rest
+
+  // https://cse.google.com/cse/create/new
+
+  console.log(data);
+
   return (
     <div className="searchPage">
       <div className="searchPageHeader">
-        <h1>{term}</h1>
+        
       </div>
+      <div className="searchPageResults">
 
-      <div className="searchPageResults"></div>
+      </div>
     </div>
   );
 };
